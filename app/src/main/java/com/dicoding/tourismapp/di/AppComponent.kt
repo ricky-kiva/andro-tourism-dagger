a230948304da10d8fa260e8478f1de7542ea3769
ricky-kiva <ricky.kiva@gmail.com>
2023-08-13 20:16:42 +0700

    feat(di-component): add DI Application Component

--- app/src/main/java/com/dicoding/tourismapp/di/AppComponent.kt
+++ app/src/main/java/com/dicoding/tourismapp/di/AppComponent.kt
@@ -6,17 +6,18 @@ import com.dicoding.tourismapp.favorite.FavoriteFragment
 import com.dicoding.tourismapp.home.HomeFragment
 import dagger.Component
 
-@AppScope
+@AppScope // define scope to Application
 @Component(
-    dependencies = [CoreComponent::class],
-    modules = [AppModule::class])
+    dependencies = [CoreComponent::class], // enable 'AppComponent' to access `CoreComponent`
+    modules = [AppModule::class]) //  module that is needed in this `AppComponent`
 interface AppComponent {
 
     @Component.Factory
     interface Factory {
-        fun create(coreComponent: CoreComponent): AppComponent
+        fun create(coreComponent: CoreComponent): AppComponent // create `AppComponent` instance with `CoreComponent` as dependency
     }
 
+    // function to choose which classes can access the dependencies
     fun inject(fragment: HomeFragment)
     fun inject(fragment: FavoriteFragment)
     fun inject(activity: DetailTourismActivity)
