50a96ce0e05cfd1950c55734b6e8d5584d8ebd9f
ricky-kiva <ricky.kiva@gmail.com>
2023-08-13 22:05:00 +0700

    feat(di): remove unused files after Hilt DI

--- app/src/main/java/com/dicoding/tourismapp/di/AppScope.kt
+++
@@ -1,7 +0,0 @@
-package com.dicoding.tourismapp.di
-
-import javax.inject.Scope
-
-@Scope // `@Scope` means this `annotation` meant to define "scope"
-@Retention(AnnotationRetention.RUNTIME) // `RUNTIME` ensure this `annotation` is "retained" & "accessible" on "runtime"
-annotation class AppScope // `annotation` is special markers that provides additional info to compiler/runtime environment
