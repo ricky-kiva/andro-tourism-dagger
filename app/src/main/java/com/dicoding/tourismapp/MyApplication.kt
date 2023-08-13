6994fa05f8d4b8195b977d042be9122bb6bb8bec
ricky-kiva <ricky.kiva@gmail.com>
2023-08-13 22:01:58 +0700

    feat(di): enable Hilt injection on Application scope

--- app/src/main/java/com/dicoding/tourismapp/MyApplication.kt
+++ app/src/main/java/com/dicoding/tourismapp/MyApplication.kt
@@ -1,19 +1,18 @@
 package com.dicoding.tourismapp
 
 import android.app.Application
-import com.dicoding.tourismapp.core.di.CoreComponent
-import com.dicoding.tourismapp.core.di.DaggerCoreComponent
-import com.dicoding.tourismapp.di.AppComponent
-import com.dicoding.tourismapp.di.DaggerAppComponent
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp // enable Application scope class injection by Hilt
 open class MyApplication: Application() {
 
+    /* Dagger without Hilt
     private val coreComponent: CoreComponent by lazy {
         DaggerCoreComponent.factory().create(applicationContext)
     }
 
     val appComponent: AppComponent by lazy {
         DaggerAppComponent.factory().create(coreComponent)
-    }
+    }*/
 
 }
