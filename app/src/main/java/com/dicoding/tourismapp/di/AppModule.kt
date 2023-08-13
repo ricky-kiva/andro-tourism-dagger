150dd58fcd909b0875483731377d1c4d80467574
ricky-kiva <ricky.kiva@gmail.com>
2023-08-13 22:03:15 +0700

    feat(di-module): configure Hilt on AppModule

--- app/src/main/java/com/dicoding/tourismapp/di/AppModule.kt
+++ app/src/main/java/com/dicoding/tourismapp/di/AppModule.kt
@@ -4,11 +4,17 @@ import com.dicoding.tourismapp.core.domain.usecase.TourismInteractor
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
+@InstallIn(ViewModelComponent::class) // decide which Hilt `Component` to use this module, which is `ViewModelComponent`
+// use `ViewModelComponent` because it only used in ViewModels
 abstract class AppModule {
 
     @Binds
+    @ViewModelScoped // set "scope" to `ViewModel`
     abstract fun provideTourismUseCase(tourismInteractor: TourismInteractor): TourismUseCase
 
 }
